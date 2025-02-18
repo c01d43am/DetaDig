97bfa9f6fceaf3a4f974076480b2f484c96e4b7a
A Pawan <alugolupawan.com>
2025-02-18 11:14:23 +0530

    Initial commit

--- DetaDig/Updates/update_checker.py
+++ DetaDig/Updates/update_checker.py
@@ -2,18 +2,25 @@ import subprocess
 
 def check_for_updates():
     """
-    Check if the local repository is up to date with the remote repository.
+    Check if the local master branch is up to date with the remote repository.
     If not, update it using git pull.
     """
     print("Checking for updates...")
     try:
-        # Check the status of the git repo
-        result = subprocess.run(['git', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        # Check the status of the git repo on the master branch
+        result = subprocess.run(['git', 'fetch'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         
-        if "Your branch is up to date" not in result.stdout:
-            print("The repository is not up to date. Pulling the latest changes...")
-            # If the repo is outdated, pull the latest changes
-            update_result = subprocess.run(['git', 'pull'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        # Check if the local master is behind the remote master
+        result = subprocess.run(['git', 'rev-parse', 'origin/master'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        remote_master_sha = result.stdout.strip()
+
+        result = subprocess.run(['git', 'rev-parse', 'master'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        local_master_sha = result.stdout.strip()
+
+        if local_master_sha != remote_master_sha:
+            print("The repository is not up to date. Pulling the latest changes from the master branch...")
+            # If the local master branch is behind, pull the latest changes
+            update_result = subprocess.run(['git', 'pull', 'origin', 'master'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             if update_result.returncode == 0:
                 print("Repository updated successfully.")
             else:
