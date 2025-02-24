f03965f203d937d8e9d25112bb879cd9d5f9bf16
A Pawan <alugolupawan.com>
2025-02-24 15:58:20 +0530

    Initial commit

--- DetaDig/Tools/extract_addresses.py
+++ DetaDig/Tools/extract_addresses.py
@@ -1,17 +1,30 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
 
-def extract_addresses(url):
+def is_valid_address(address):
+    common_suffixes = ["Street", "St", "Avenue", "Ave", "Boulevard", "Blvd", "Road", "Rd", "Lane", "Ln", "Drive", "Dr", "Court", "Ct", "Way", "Place", "Pl"]
+    return any(suffix in address for suffix in common_suffixes)
+
+def extract_addresses(url, visited=set()):
+    if url in visited:
+        return set()
+    
+    visited.add(url)
     response = requests.get(url)
     if response.status_code != 200:
-        return "Failed to retrieve website"
+        return set()
     
     soup = BeautifulSoup(response.text, 'html.parser')
     page_text = soup.get_text()
     
-    address_pattern = r'\d{1,5}\s\w+\s\w+'
-    addresses = set(re.findall(address_pattern, page_text))
+    address_pattern = r'\d{1,5}\s\w+(?:\s\w+)*\s(?:Street|St|Avenue|Ave|Boulevard|Blvd|Road|Rd|Lane|Ln|Drive|Dr|Court|Ct|Way|Place|Pl)'
+    addresses = set(filter(is_valid_address, re.findall(address_pattern, page_text)))
     
-    return addresses if addresses else "No addresses found"
- 
+    for link in soup.find_all('a', href=True):
+        next_url = urljoin(url, link['href'])
+        if urlparse(next_url).netloc == urlparse(url).netloc:
+            addresses.update(extract_addresses(next_url, visited))
+    
+    return addresses if addresses else {"No addresses found"}
